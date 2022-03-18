37d7496c9c1502597021a577fdbd0ffacfb8318d
Derek <derekbelloni10@gmail.com>
2022-03-18 17:14:52 -0600

    init

---
+++ Tower-vue.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-6qxe8r5c.us.auth0.com'
+export const clientId = '91vHcWtuio22K3YBLHNPHb5WD4GTpUh9'
+export const audience = 'https://derek-dev.com'
